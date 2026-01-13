f69b741129d9d1df27564634e4f12567cbd25007
Vahe <148644489+griarsvah@users.noreply.github.com>
2026-01-13 16:32:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,25 @@
-// This is the "Offline page" service worker
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "pwabuilder-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+
+
+
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
